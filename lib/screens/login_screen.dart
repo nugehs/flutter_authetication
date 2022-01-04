7fc020476d82fbfaf665c6893c4498af6a11ed66
Segun Olumbe <david.olumbe@gmail.com>
2022-01-04 23:56:29 +0000

    add register screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/screens/registration_screen.dart';
+import 'package:flutterfire_ui/auth.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Login',
-          style: TextStyle(fontSize: 30),
-        ),
-      ),
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
+          return const SignInScreen(providerConfigs: [
+            EmailProviderConfiguration(),
+          ]);
+        }
+
+        // Render your application if authenticated
+        return const RegistrationScreen();
+      },
     );
   }
 }
