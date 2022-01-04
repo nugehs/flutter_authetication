10c6f00e84258d4188017e79047dd1be27dd4a36
Segun Olumbe <david.olumbe@gmail.com>
2022-01-04 23:38:54 +0000

    setup firebase config

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
           context,
           MaterialPageRoute(
